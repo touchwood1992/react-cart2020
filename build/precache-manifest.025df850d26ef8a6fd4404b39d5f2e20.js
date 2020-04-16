3b1654a7dfa9e69cd2a8ee5638a0c9e6a8469ea7
touchy <prashantgecg@gmail.com>
2020-04-16 19:27:29 +0530

    build folder push

---
+++ build/precache-manifest.025df850d26ef8a6fd4404b39d5f2e20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9016bab1f2ff8338cc705a2ebf687c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfb2310e2a6eeda52b2",
+    "url": "/static/css/2.90d84c0a.chunk.css"
+  },
+  {
+    "revision": "b301f78e7df963a98487",
+    "url": "/static/css/main.62d75c3c.chunk.css"
+  },
+  {
+    "revision": "3dfb2310e2a6eeda52b2",
+    "url": "/static/js/2.fa0d76bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fa0d76bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b301f78e7df963a98487",
+    "url": "/static/js/main.43f45a0e.chunk.js"
+  },
+  {
+    "revision": "26d484170b1ab1d29375",
+    "url": "/static/js/runtime-main.4bc7c12e.js"
+  }
+]);
